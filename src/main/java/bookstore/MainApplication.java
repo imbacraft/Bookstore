b4db09d281514de04559b50352d0f1155337f7e4
Dimitris Bouzoukas <dimbouzoukas@gmail.com>
2021-03-26 16:45:42 +0200

    removed javafx import from mainclass

--- src/main/java/bookstore/MainApplication.java
+++ src/main/java/bookstore/MainApplication.java
@@ -1,23 +1,14 @@
 package bookstore;
 
 
-
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication extends SpringBootServletInitializer {
+public class MainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
         
-                 @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-      return builder.sources(Application.class);
-  }
-
 }
