58c52583d98f6dffcef26903b0257ec7a4b824dd
Georgios Tsalikis <geocode@tsalikis.net>
2021-04-04 01:19:00 +0300

    * Made a second change in mapping.
    PostMapping now works

--- src/main/java/bookstore/controller/RegistrationController.java
+++ src/main/java/bookstore/controller/RegistrationController.java
@@ -42,7 +42,7 @@ public class RegistrationController {
         
         //If there is a problem, show registration form again
         if(result.hasErrors()){
-            return "register-form";
+            return "register";
         }
         
         //save customer  to DB
